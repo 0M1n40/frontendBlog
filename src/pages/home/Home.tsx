8ab3d448bf0c4da7297190ab619967cffa57873b
Mina Yura <minammonteiro2@gmail.com>
2025-04-02 18:29:57 -0300

    Configurações Home

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,38 @@
+
+
+function Home() {
+  return (
+    <>
+      <div>
+        <div
+          style={{
+            width: "80vw",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <h2>Seja Bem vindo!</h2>
+          <p>Expresse aqui seus pensametos e opiniões</p>
+
+          <div
+            style={{
+              width: "80vw",
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
+          >
+            <img
+              src="https://i.imgur.com/VpwApCU.png"
+              alt="Imagem da Página Home"
+              width="400px"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
