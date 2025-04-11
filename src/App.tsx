79056ea3b5e26722a7f8e48e4c51988ca661d99d
Mina Yura <minammonteiro2@gmail.com>
2025-04-11 16:57:10 -0300

    Até o cookbook 31

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Footer from './components/footer/Footer';
-import Navbar from './components/navbar/Navbar';
-import Home from './pages/home/Home';
-import Cadastro from './pages/cadastro/Cadastro';
-import Login from './pages/login/Login';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
+import { AuthProvider } from "./contexts/AuthContext";
+import Home from "./pages/home/Home";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Login from "./pages/login/Login";
+import ListaTemas from "./components/temas/listatemas/ListaTemas";
+import FormTema from "./components/temas/formtema/FormTema";
+import DeletarTema from "./components/temas/deletartema/DeletarTema";
+import ListaPostagens from "./components/postagens/listapostagens/ListaPostagens";
+import FormPostagem from "./components/postagens/formpostagem/FormPostagem";
+import DeletarPostagem from "./components/postagens/deletarpostagem/deletarpostagem";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <div className="min-h-[80vh]">
-          <Routes>
-          <Route path="/" element={<Login />} />
+      <AuthProvider>
+        <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap"
+          rel="stylesheet"
+        />
+        <BrowserRouter>
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
-          </Routes>
-        </div>
-        <Footer />
-      </BrowserRouter>
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+              <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+              <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
     </>
   );
 }
